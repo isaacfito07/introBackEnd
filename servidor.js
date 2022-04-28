d24642f79ff7e11511066498a8b09fb9cd3cf2f7
isaacfito07 <isaacfito07@gmail.com>
2022-04-28 11:51:33 -0600

    Se agrego un archivo html del error 404 y se codifico su ejecucion

--- servidor.js
+++ servidor.js
@@ -21,5 +21,22 @@ http.createServer(function(request, response) {
         '.gif': 'image/gif'
     }
 
+    fs.readFile(filePath,function(error, content){
+        if(error){
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType});
+                    response.end(content, 'utf8');
+                });
+            }else{
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error)
+                response.end();
+            }
+        }else{
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
     
 });
